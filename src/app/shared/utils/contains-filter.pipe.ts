6906fef9b8b603f8927335057652fc2cc24fb7b8
Maloth Naresh <idofmalothnaresh@gmail.com>
2017-11-22 20:50:07 +0530

    Removed toLowercase function

--- src/app/shared/utils/contains-filter.pipe.ts
+++ src/app/shared/utils/contains-filter.pipe.ts
@@ -9,6 +9,6 @@ export class ContainsFilterPipe implements PipeTransform {
         if (!items || !filter) {
             return items;
         }
-        return items.filter(item => item.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+        return items.filter(item => item.name.indexOf(filter) !== -1);
     }
 }
