6fe99ec9e150c2cc6ab7bb68ee43d70527e0eebc
Maloth Naresh <idofmalothnaresh@gmail.com>
2018-01-23 01:42:22 +0530

    Added new files for service classes

---
+++ src/app/product/product-form/product.model.ts
@@ -0,0 +1,18 @@
+export interface Product {
+  name: string;
+  batchId: string;
+  manufacture: string;
+  expire: string;
+  manOrMachine: string;
+  goldCarat: string;
+  percentChrg: string;
+  grossWeight: string;
+  netWeight: string;
+  stoneWeight: string;
+  beadsWeight: string;
+  perGramWeight: string;
+  heading: string;
+  country: string;
+  city: string;
+  description: string;
+}
---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class APIService {
+
+  private headers: any = {};
+  constructor(private http: Http) { 
+    this.headers.Authorization = localStorage.getItem('Authorization');
+  }
+  
+
+  getAll(url: string): Observable<any> {
+    return this.http.get(url, {
+      headers: this.headers
+    }).map(res => {
+      return res.json();
+    });
+  }
+
+  getOne(url: string, id: string): Observable<any> {
+    return this.http.get(url + id).map(res => {
+      return res.json();
+    });
+  }
+
+  createOne(url: string, data: any): Observable<any> {
+    return this.http.post(url, data).map(res => {
+      return res.json();
+    });
+  }
+
+  updateOne(url: string, data: any): Observable<any> {
+    return this.http.put(url, data).map(res => {
+      return res.json();
+    });
+  }
+
+}
