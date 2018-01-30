57739a9941f157f963039797cf7f4832384372d9
Maloth Naresh <idofmalothnaresh@gmail.com>
2018-01-31 00:22:06 +0530

    Changes for user, products JWT authentication

--- src/app/user/provider/user-data.service.ts
+++ src/app/user/provider/user-data.service.ts
@@ -25,7 +25,6 @@ export class UserDataService {
     return this.apiService.getAll(this.listUrl).map( res => {
       return res;
     });
-    // return this.http.get(this.listUrl).map(res => res.json());
   }
 
   delete(user: any) {
